f3af804dd474e60979cfe445c341d580bb547285
Marcos ASL <Mark.ASL@hotmail.com>
2022-12-22 09:53:59 -0300

    2 ok

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function trueoufalse(valor) {
   return !!valor;
 }
+
+/*Crie uma function matemática que retorne o perímetro de um quadrado, obs: perímetro é a soma dos quatro lados do quadrado*/
+function perímetro(lado) {
+  return lado * 4;
+}
