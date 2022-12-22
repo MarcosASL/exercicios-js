b7f5816978cc936e5e546de29ff3f2969d0265f4
Marcos ASL <Mark.ASL@hotmail.com>
2022-12-22 20:34:46 -0300

    feat: exercicio 14

--- script.js
+++ script.js
@@ -176,3 +176,15 @@ function livro(nome, ano, autor) {
 }
 const retornotudo = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
 console.log(retornotudo.frase);
+
+/*----------------------------------------------------------------------------------- */
+// 1 - Crie um círculo com uma div de width: 16px, height: 16px e background: #999;
+// 2 - Adicione ao window uma função que ocorre ao "mousemove";
+// 3 - Na função mude as propriedades top e left do círculo com base no mouse.
+// 4 - Você pode usar elemento.style.propriedade para mudar o CSS diretamente.
+const circulo = document.querySelector("#circulo");
+function seguirmouse(event) {
+  circulo.style.top = event.y + "px";
+  circulo.style.left = event.x + "px";
+}
+window.addEventListener("mousemove", seguirmouse);
