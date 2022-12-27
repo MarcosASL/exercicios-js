648155bcc6836455d24b353f91a93bd8a78ed696
Marcos ASL <Mark.ASL@hotmail.com>
2022-12-27 11:03:12 -0300

    feat: exercicio 15

--- script.js
+++ script.js
@@ -193,3 +193,29 @@ function seguirmouse(event) {
   circulo.style.left = event.x + "px";
 }
 window.addEventListener("mousemove", seguirmouse);
+
+/*----------------------------------------------------------------------------------- */
+
+/*1 - Crie um botão com nome de Adicionar.
+2 - Adicione uma função (somar) ao clique desse botão
+3 - Só adicione a função se o botão existir na tela.
+4 - Crie uma div com o texto 0 dentro dela: <div>0</div>
+5 - Na função do botão, pegue o total que estiver dentro da div e adicione + 1.
+6 - Mude o valor da div para o novo total.
+7 - Mude o valor apenas se o total for menor que 10.
+8 - Quando não for mais possível adicionar, mostre uma mensagem no console. */
+
+const botao = document.querySelector("#b-botao");
+
+function somar() {
+  const div = document.querySelector("#div-zero");
+  const total = Number(div.innerText) + 1;
+  if (total < 10) {
+    div.innerText = total;
+  } else {
+    console.log("Não é possivel adicionar mais");
+  }
+}
+if (botao) {
+  botao.addEventListener("click", somar);
+}
