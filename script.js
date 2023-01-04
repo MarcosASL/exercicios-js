e2d6ee85b79b41ee6c1f6e254523b9ffd6eb9596
Marcos ASL <Mark.ASL@hotmail.com>
2023-01-03 22:46:01 -0300

    feat: execicio rev 1

--- script.js
+++ script.js
@@ -273,6 +273,10 @@ imgs.forEach(() => i++);
 /*------------------JÁ FAZ PARTE DAS NOVA AULAS------------------------ */
 
 // Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll(".menu a");
+itensMenu.forEach((item) => {
+  item.classList.add("ativo");
+});
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
